11775959e34671f84f0addb3c783c1d16f2559de
EvaFro <frolli.studios@gmail.com>
2018-05-17 14:21:12 -0700

    add paintBucket prompt to repo

---
+++ paintBucket/Solutions/paintBucket.js
@@ -0,0 +1,24 @@
+/** Implement the paint bucket function in Paint. When you click on a pixel in an image, 
+ * it overrides all adjacent pixels of the same color with the new specified color Input: 
+ * “Image” is represented by an n x m matrix of colors, for example: [ [ ‘pink’, pink, ‘red’], 
+ * [ ‘pink’, blue, pink] ] x and y represent coordinates in grid (like pixels on a screen) - 
+ * for example, x=0, y=0 Color with which to override original color, for example: ‘blue’ Output: 
+ * Returns grid with original colors Output: Returns grid with original colors overwritten, 
+ * based on example inputs above, [ [‘blue’, ‘blue’, ‘red’], [‘blue’, ‘blue’, ‘pink’] ] 
+ * 
+ * Assumptions (fyi these were not given by the interviewer): colors are all represented the same way,
+ * so you won’t see “#fff”/“white”/rgba(255,255,255) which are equivalent colors. 
+ * Diagonals are not considered adjacent. 
+ * You can return either the same array or a new array (pick whichever is easier for you) 
+ * When your input is outside the m x n grid, return the grid with no colors changed **///
+
+ function paintBucket (grid, x, y, color) {
+
+ }
+
+
+   
+   
+// Test
+var image = [ [ 'pink', 'pink', 'red'], [ 'pink', 'blue', 'pink'], [ 'pink', 'pink', 'pink']];
+console.log(paintBucket(image, 0, 0, "blue")); // [ [ 'blue', 'blue', 'red'], [ 'blue', 'blue', 'blue'], [ 'blue', 'blue', 'blue']];
